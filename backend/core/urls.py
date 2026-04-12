829d10de469163c7e94e70828cc76422f209f91a
jeanxzdev <jeanxzdev@users.noreply.github.com>
2026-04-12 13:28:44 -0500

    fix(media): serve media files in production

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +14,9 @@ urlpatterns = [
     path('api/anti-fraud/', include('apps.anti_fraud.urls')),
 ]
 
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
