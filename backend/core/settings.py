a2bda7d7629ab13f626a5083aa0a8c99e444d8c7
jeanxzdev <jeanxzdev@users.noreply.github.com>
2026-04-13 23:00:50 -0500

    fix: add STATICFILES_STORAGE back to prevent cloudinary-storage from crashing

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
