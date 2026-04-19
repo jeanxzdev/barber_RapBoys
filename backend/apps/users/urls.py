0864fe0597f0cfdb5df6f3ed6dd27b853189f664
jeanxzdev <jeanxzdev@users.noreply.github.com>
2026-04-19 10:10:57 -0500

    con claude usuario

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import RegisterView, UserDetailView, UserViewSet, crear_admin_temporal 
 from .views import crear_admin_temporal
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,15 +7,12 @@ from rest_framework_simplejwt.views import (
 )
 
 router = DefaultRouter()
-# router.register(r'list', UserViewSet, basename='user')
 
 urlpatterns = [
-    # 2. Agrega la ruta secreta aquí arriba
     path('setup-admin-2026/', crear_admin_temporal), 
-    
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', UserDetailView.as_view(), name='profile'),
+    # path('register/', RegisterView.as_view(), name='register'),  # COMENTADAS POR AHORA
+    # path('profile/', UserDetailView.as_view(), name='profile'),  # COMENTADAS POR AHORA
     path('', include(router.urls)),
 ]
