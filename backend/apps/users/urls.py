b827282257638bdfcc3183015b753216a8f1a4b9
jeanxzdev <jeanxzdev@users.noreply.github.com>
2026-04-19 10:31:19 -0500

    importando

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import crear_admin_temporal
+from .views import crear_admin_temporal, RegisterView, UserDetailView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,7 +12,7 @@ urlpatterns = [
     path('setup-admin-2026/', crear_admin_temporal), 
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register'),  # COMENTADAS POR AHORA
-    path('profile/', UserDetailView.as_view(), name='profile'),  # COMENTADAS POR AHORA
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UserDetailView.as_view(), name='profile'),
     path('', include(router.urls)),
 ]
